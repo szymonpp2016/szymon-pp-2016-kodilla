1daaa6ac73442fa572e01407140bbd75b7550883
szymonpp2016 <--global>
2018-01-18 13:53:16 +0100

    modul_6_5

---
+++ src/main/java/com/kodilla/testing/forum/statistics/Statistics.java
@@ -0,0 +1,10 @@
+package com.kodilla.testing.forum.statistics;
+import java.util.List;
+
+public interface Statistics {
+
+ public  List<String> usersNames(); // list of users names
+    public int postsCount();          // total quantity of forum posts
+    public int commentsCount();       // total quantity of forum comments
+
+}
