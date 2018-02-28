2ef3cc432e9a0f3c95859c5bc723510f518ebc35
szymonpp2016 <szymon.pp.2016@interia.pl>
2018-02-28 23:07:58 +0100

    modul_8_1

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
@@ -0,0 +1,26 @@
+package com.kodilla.exception.test;
+
+public class FirstChallenge {
+
+    private double divide(double a, double b) throws ArithmeticException {
+        if(b == 0){
+            throw new ArithmeticException();
+        }
+        return a / b;
+    }
+
+    public static void main(String[] args) {
+
+        FirstChallenge firstChallenge = new FirstChallenge();
+        double result;
+        try {
+            result = firstChallenge.divide(3, 0);
+            System.out.println(result);
+        } catch(ArithmeticException e) {
+            System.out.println("Pamiętaj `cholero` nie dziel przez zero!!");
+        }
+        finally {
+            System.out.println("Operacja zakończona ");
+        }
+    }
+}
