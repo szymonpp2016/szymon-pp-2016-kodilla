de9b23f05fd5216c549b92f4615cbad2cbbbf3c4
szymonpp2016 <szymon.pp.2016@interia.pl>
2018-04-27 12:06:29 +0200

    modul_20_3_ASPECT

---
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/facade/api/OrderFacadeWatcher.java
@@ -0,0 +1,18 @@
+package com.kodilla.patterns2.facade.api;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class OrderFacadeWatcher {
+    private static final Logger LOGGER = LoggerFactory.getLogger(OrderFacadeWatcher.class);
+
+    @Before("execution(* com.kodilla.patterns2.facade.api.OrderFacade.processOrder(..)) && args(*,orderId))")
+    public void logEvent(Long orderId) {
+        LOGGER.info("The method processorOrder was use. Order processing (ID): " + orderId);
+    }
+}
