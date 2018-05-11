4db0810cdde9fbfbb48e59623a935b2c06baddba
szymonpp2016 <szymon.pp.2016@interia.pl>
2018-05-11 12:30:42 +0200

    23.1_TEST_LOOP

---
+++ kodilla-testing/src/test/java/com/kodilla/testing/loop/testLoopAndConditionalBreakpoints.java
@@ -0,0 +1,23 @@
+package com.kodilla.testing.loop;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class testLoopAndConditionalBreakpoints {
+
+
+@Test
+   public void testLoop(){
+
+    long sum=0;
+
+    for (int n=0; n<1000; n++) {
+        sum +=n;
+        System.out.println("for n:"+n+" sum:"+sum);
+
+    }
+    assertEquals(499500,sum);
+
+}
+}
